1bfa4cbf3e81e871420fe0ae8dbe5a758db76a0f
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-03-31 23:02:00 +0200

    Adds cube movement and rotation

---
+++ Assets/Scripts/FreeMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+    private float speed = 5;
+    
+    private GameObject texture;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            MovePlayer(Vector3.up);
+
+        if (Input.GetKey(KeyCode.A))
+            MovePlayer(Vector3.left);
+
+        if (Input.GetKey(KeyCode.S))
+            MovePlayer(Vector3.down);
+
+        if (Input.GetKey(KeyCode.D))
+            MovePlayer(Vector3.right);  
+    }
+
+
+    private void MovePlayer(Vector3 direction)
+    {
+        transform.Translate(Time.deltaTime * speed * direction);
+    }
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    private bool isRotating;
+    private Vector3 originRot, targetRot;
+    private float timeToRotate = 0.2f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q) && !isRotating)
+            StartCoroutine(RotatePlayer(-1));
+
+        if (Input.GetKey(KeyCode.E) && !isRotating)
+            StartCoroutine(RotatePlayer(1));
+    }
+
+    private IEnumerator RotatePlayer(float direction)
+    {
+        isRotating = true;
+        float elapsedTime = 0;
+
+        transform.Rotate(90 * direction * Vector3.forward);
+
+        while (elapsedTime < timeToRotate)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isRotating = false;
+    }
+}
