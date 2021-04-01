382db7cd0229e0b573c9f8ae36247a7e4751ecc3
WontresGit <marcin.karcz@ufl.edu>
2021-03-31 22:29:41 -0400

    Chcialem cos ogarnac z laczeniem bloczkow ale RiP

---
+++ Assets/Scripts/Connect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connect : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        this.transform.parent = coll.gameObject.transform;
+        Debug.Log("Czemu to nie dziala?");
+    }
+
+    //void OnTriggerEnter2D(Collider2D coll)
+    //{
+    //    this.transform.parent = coll.gameObject.transform;
+    //    Debug.Log("Czemu to nie dziala?");
+    //}
+}
