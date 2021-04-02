5ce3ae1d0c0940e7689c45cf063bccfedb08255d
Albert Jendrzejczak <aweczet@gmail.com>
2021-04-02 22:24:59 +0200

    add: door movement script

---
+++ Assets/Scripts/DoorsMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class DoorsMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = transform.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _rigidbody2D.MovePosition(transform.position + Vector3.up);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            _rigidbody2D.MovePosition(transform.position + Vector3.down);
+        }
+    }
+}
